bb114255f666254dc0d73ca1e9ee6e3d567a222d
bubunyun <171100337+bubunyun@users.noreply.github.com>
2025-03-10 16:00:07 +0000

    add extern objects

--- wrapper_classes/hm05.h
+++ wrapper_classes/hm05.h
@@ -1,10 +1,16 @@
 #include "mbed.h"
+#include "Menu.h"
+#include "C12832.h"
+
+extern Menu menu;
+extern BufferedSerial hm10, pc;
+extern C12832 lcd;
 
 void runHM05Test() {
     // Application buffer to receive the data
     char buf[32] = {0};
 
-    while (!menu.exitRequest()) {
+    while (!menu.exitRequested()) {
         if (hm10.readable()) {
             uint32_t num = hm10.read(buf, sizeof(buf));
             lcd.write(buf, num);
